c85bdb7a082962b08426fcf4f04454ff5711e1c4
romain-mezghenna <romain.mezghenna@etu.umontpellier.fr>
2022-02-25 08:08:17 +0100

    améliorations lazy loading et bouton pour charger l'ensemble des pokemons

--- main.js
+++ main.js
@@ -7,21 +7,27 @@ var index = 0;
 var colorSpecies = []
 var timer = false
 
-// builds the order of the pokemon
-async function getPokemons(){
-    let limit = index+25
-    while(index<limit){
+// Get n pokemons details on the pokemon list 
+async function getPokemons(n){
+    timer = false
+    let limit = index+n
+    while (index < limit) {
+        if (index < list.length) {
             let _ = await P.resource(list[index].url).then(async res => {
                 let _ = await P.resource(res.species.url).then(response => {
                     res.color = response.color.name
                     detailsList.push(res)
                 })
-                
             })
-        index++
+            index++
+        }
     }
     timer = true
+    if(detailsList.length == list.length){
+        alert("Tous les pokemons ont été chargés !")
+    }
 }
+
 
 var app = new Vue({
     el: '#app',
@@ -41,16 +47,15 @@ var app = new Vue({
 // Get the full list of the pokemons and request for all the data progressively
 P.getPokemonsList().then(res => {
     list = res.results
-    getPokemons()
+    getPokemons(30)
 })
 
 document.onscroll = function(){
     if(timer){
         var h = window.outerHeight
         if (window.scrollY + h > document.body.scrollHeight) {
-            getPokemons()
+            getPokemons(30)
             timer = false
         }
-
     }
 }
